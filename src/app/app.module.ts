a638877bb9a4cd19574f7fab52ea513039c6d6cd
Nadi11235 <nadi112355@gmail.com>
2020-07-26 18:07:10 +0300

    first javaScript excersice for components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component'
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainLayoutComponent } from './mainLayout/mainLayout.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ContentComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainLayoutComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/mainLayout/mainLayout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-main-layout',
+    styleUrls: ['./mainlayout.component.css'],
+    templateUrl: './mainLayout.component.html'
+})
+export class MainLayoutComponent{
+
+}
